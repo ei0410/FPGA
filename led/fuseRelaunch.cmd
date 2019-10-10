269d943f6727fc6e69158fd4740e5f608024774c
ei0410 <chronolavos0124@gmail.com>
2019-10-10 17:38:37 +0900

    delete all file except .v and .ucf

--- led/fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -lib "secureip" -o "C:/Xilinx/FPGA/led/testOR_isim_beh.exe" -prj "C:/Xilinx/FPGA/led/testOR_beh.prj" "work.testOR" "work.glbl" 
